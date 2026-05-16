690ee58fd1d54d95eee5eb128892693983788dea
Lei Huang <48151038+Winlere@users.noreply.github.com>
2026-05-15 22:31:10 -0500

    Refactor Gather class to avoid onnx::if arisen from squeeze
    Refactor Gather class to improve index handling and selection logic.

--- onnx2pytorch/operations/gather.py
+++ onnx2pytorch/operations/gather.py
@@ -4,17 +4,26 @@ from torch import nn
 
 class Gather(nn.Module):
     def __init__(self, dim=0):
+        super().__init__()
         self.dim = dim
         self.selection = [slice(None) for _ in range(dim)]
-        super().__init__()
 
     def forward(self, data: torch.Tensor, indices: torch.Tensor):
-        selection = self.selection + [indices.to(torch.int64)]
+        indices = indices.to(torch.int64)
 
         if indices.numel() == 1 and indices == -1:
-            indices = torch.tensor(data.shape[self.dim] - 1, device=data.device)
+            indices = torch.tensor(
+                data.shape[self.dim] - 1,
+                device=data.device,
+                dtype=torch.int64,
+            )
 
         if indices.ndim == 0:
-            return torch.index_select(data, dim=self.dim, index=indices).squeeze(self.dim)
+            out = torch.index_select(
+                data,
+                dim=self.dim,
+                index=indices.reshape(1),
+            )
+            return out.select(self.dim, 0)
 
         return torch.index_select(data, dim=self.dim, index=indices)
