3370c1a75c2b1e5d4dbabb6139142966be75db07
Lei Huang <48151038+Winlere@users.noreply.github.com>
2026-05-16 16:19:08 -0500

    Refactor Gather class for improved clarity and efficiency

--- onnx2pytorch/operations/gather.py
+++ onnx2pytorch/operations/gather.py
@@ -4,26 +4,17 @@ from torch import nn
 
 class Gather(nn.Module):
     def __init__(self, dim=0):
-        super().__init__()
         self.dim = dim
         self.selection = [slice(None) for _ in range(dim)]
+        super().__init__()
 
     def forward(self, data: torch.Tensor, indices: torch.Tensor):
-        indices = indices.to(torch.int64)
+        selection = self.selection + [indices.to(torch.int64)]
 
         if indices.numel() == 1 and indices == -1:
-            indices = torch.tensor(
-                data.shape[self.dim] - 1,
-                device=data.device,
-                dtype=torch.int64,
-            )
+            indices = torch.tensor(data.shape[self.dim] - 1, device=data.device)
 
         if indices.ndim == 0:
-            out = torch.index_select(
-                data,
-                dim=self.dim,
-                index=indices.reshape(1),
-            )
-            return out.select(self.dim, 0)
+            return torch.index_select(data, dim=self.dim, index=indices.reshape(1)).select(self.dim, 0)
 
         return torch.index_select(data, dim=self.dim, index=indices)
